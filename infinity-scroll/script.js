7707508fecb1c40144ac5181d3494e02720e8e87
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2023-11-26 23:00:45 +0530

    Make code DRY

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -8,18 +8,27 @@ const apiKey = "hgi7tnqahAD5EQYIEV83Rls5NfT0Upsjmq-I-N3h00Q";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // set attributes
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
 
 // display photos
 function displayPhoto() {
     photosArray.forEach((photo) => {
         const item = document.createElement("a");
-        item.setAttribute("href", photo.links.html);
-        item.setAttribute("target", "_blank");
+        setAttributes(item, {
+            href: photo.links.html,
+            target: "_blank",
+        });
 
         const image = document.createElement("img");
-        image.setAttribute("src", photo.urls.regular);
-        image.setAttribute("alt", photo.description);
-        image.setAttribute("title", photo.description);
+        setAttributes(image, {
+            src: photo.urls.regular,
+            alt: photo.description,
+            title: photo.description,
+        });
 
         item.appendChild(image);
         imageContainer.appendChild(item);
@@ -37,3 +46,4 @@ async function getApi() {
 }
 
 getApi();
+
