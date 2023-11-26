04097f943c426a5b59650619d5662eb27739c2cd
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2023-11-26 22:58:52 +0530

    Display photos

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -8,32 +8,18 @@ const apiKey = "hgi7tnqahAD5EQYIEV83Rls5NfT0Upsjmq-I-N3h00Q";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
 // set attributes
-function setAttributes(element, attributes) {
-    for (const key in attributes) {
-        element.setAttribute(key, attributes[key]);
-    }
-}
 
 // display photos
 function displayPhoto() {
     photosArray.forEach((photo) => {
         const item = document.createElement("a");
-        // item.setAttribute("href", photo.links.html);
-        // item.setAttribute("target", "_blank");
-        setAttributes(item, {
-            href: photo.links.html,
-            target: "_blank",
-        });
+        item.setAttribute("href", photo.links.html);
+        item.setAttribute("target", "_blank");
 
         const image = document.createElement("img");
-        // image.setAttribute("src", photo.urls.regular);
-        // image.setAttribute("alt", photo.description);
-        // image.setAttribute("title", photo.description);
-        setAttributes(image, {
-            src: photo.urls.regular,
-            alt: photo.description,
-            title: photo.description,
-        });
+        image.setAttribute("src", photo.urls.regular);
+        image.setAttribute("alt", photo.description);
+        image.setAttribute("title", photo.description);
 
         item.appendChild(image);
         imageContainer.appendChild(item);
@@ -51,10 +37,3 @@ async function getApi() {
 }
 
 getApi();
-
-// scroll 
-window.addEventListener('scroll', ()=>{
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
-        displayPhoto
-    }
-})
